1f5d70e2e19520f04083564b72148b0d015796ab
Pro Sec <pt@prosec-networks.com>
2021-04-28 15:58:30 +0200

    Script erweiter. Automatische Tests Ext -> Int; Int -> Int; Int -> Ext; Ext -> Ext

--- mail_test/mail_test.sh
+++ mail_test/mail_test.sh
@@ -7,11 +7,9 @@ echo '| |   | | | (_) |___) |  __/ (__  | |  | | (_| | | | ||  __/\__ \ |_ '
 echo '|_|   |_|  \___/_____/ \___|\___| |_|  |_|\__,_|_|_|\__\___||___/\__|'
 echo '                                                                     '
 
-read -p "Sender Email: " from
+read -p "External Mail: "  ext
 echo "  "
-read -p "Header From field: " field
-echo " "
-read -p "Receiver Email: " rcpt
+read -p "Internal Mail: " int
 echo " "
 read -p "Subject: " sub
 echo " "
@@ -20,4 +18,16 @@ echo " "
 read -p "IP Address SMTP: " ip
 echo " "
 echo " "
-swaks --ehlo mx.team-prosec.com --from $from --h-From: "$field" --h-Subject: "$sub" --body "$data" --to $rcpt --server $ip -tlso
+echo "First check: Extern -> Intern"
+swaks --ehlo mx.team-prosec.com --from $ext --h-From: "$ext" --h-Subject: "$sub" --body "$data" --to $int --server $ip -tlso > ext_to_int.txt
+echo " "
+echo "Second check: Intern -> Intern"
+swaks --ehlo mx.team-prosec.com --from $int --h-From: "$int" --h-Subject: "$sub" --body "$data" --to $int --server $ip -tlso > int_to_int.txt
+echo " "
+echo "Third check: Intern -> Extern"
+swaks --ehlo mx.team-prosec.com --from $int --h-From: "$int" --h-Subject: "$sub" --body "$data" --to $ext --server $ip -tlso > int_to_ext.txt
+echo " "
+echo "Fourth check: Extern -> Extern"
+swaks --ehlo mx.team-prosec.com --from $ext --h-From: "$ext" --h-Subject: "$sub" --body "$data" --to $ext --server $ip -tlso > int_to_ext.txt
+echo " "
+echo "Checks performed. Check output files for more information"
