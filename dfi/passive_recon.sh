ad62ef74ce68907f95da27d7ffaa1c6b555a69a3
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-05 14:38:35 +0000

    Add new file

---
+++ dfi/passive_recon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmux new-session -d
+tmux send 'netdiscover -L -i eth0 > /root/output/netdiscover'
+tmux split-window
+tmux send 'python3 /opt/PCredz/Pcredz -i eth0 -c' ENTER
+tmux split-window
+tmux send 'tail --follow /root/output/netdiscover' ENTER
+tmux a
