0dd353d0b0bc52ee1233d3ec0c4ad863e4566222
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-06 06:31:00 +0000

    create folder IF

--- dfi/passive_recon.sh
+++ dfi/passive_recon.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[[ -d /root/output/nmap && -d /root/output/list && -d /root/input/msf ]]]; then
+    echo 'Ordner sind da!'
+else    
+    #Creating Output Folders
+    mkdir -p /root/output/nmap /root/output/list /root/input/msf
+fi
+
 tmux -f dfitmux.conf new-session -d
 tmux rename-window 'Passive-Recon'
 tmux send 'netdiscover -L -i eth0 > /root/output/netdiscover' ENTER
