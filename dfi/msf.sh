bd6c42366a60e2cbee05b0714e488c7209f642c7
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-06 07:03:21 +0000

    change to english

--- dfi/msf.sh
+++ dfi/msf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ -s ./resource.txt ]; then
-    echo 'resource.txt ist da'
+    echo '! > resource.txt check OK'
 else
-    echo 'resource.txt fehlt hier: ' $PWD
+    echo '! > resource.txt missing here: ' $PWD
     exit 1
 fi
 
 IP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6)
-echo 'eigene IP eth0: '$IP
+echo '! >> own IP eth0: '$IP
 
 if [[ -d /root/output/msf  && -d /root/input/msf ]]; then
-    echo 'Ordner sind da!'
+    echo '! > Folder exist!'
 else    
     #Creating Output Folders
     mkdir -p /root/output/msf
@@ -20,7 +20,7 @@ else
     mkdir -p /root/input/msf
 fi
 
-read -p "Workspace eingeben: " WS
+read -p "Enter Workspace Name: " WS
 
 echo $WS
 
@@ -28,4 +28,6 @@ echo "workspace -d $WS" > /root/input/msf/workspace.txt
 echo "workspace -a $WS" >> /root/input/msf/workspace.txt
 echo "db_import /root/output/nmap/service.xml" >> /root/input/msf/workspace.txt
 
+echo '! > Start Metasploit Framework'
 msfconsole -x "resource /root/input/msf/workspace.txt resource ./resource.txt"
+echo '! > Done!'
