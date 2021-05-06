4883304fb0078a7dfa718af25ebfb90e940e9ebb
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-06 06:58:09 +0000

    change to english

--- dfi/eyewitness.sh
+++ dfi/eyewitness.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -d /root/output/screens ]]; then
-    echo 'Ordner sind da; mv zu screens_old !'
+    echo '! > Folder exist; mv screens screens_old for Backup purpose!'
     mv /root/output/screens /root/output/screens_old
     mkdir -p /root/output/screens
 else    
     #Creating Output Folders
     mkdir -p /root/output/screens
-    echo 'Ordner erstellt'
+    echo '! > Folder created!'
 fi
 
 eyewitness --web --timeout --delay 20 --no-prompt --prepend-https -x /root/output/nmap/service.xml -d /root/output/screens/
