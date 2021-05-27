6566ddb370cc8099b56d418c8379a9ddbe66eb9d
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-05-27 15:23:01 +0000

    Added Banner + made MSF Silent

--- dfi/msf.sh
+++ dfi/msf.sh
@@ -7,11 +7,21 @@ else
     exit 1
 fi
 
+echo -e '  _____           _____                         _        _____       _       _ _   '
+echo -e ' |  __ \         / ____|             /\        | |      / ____|     | |     (_) |  '
+echo -e ' | |__) | __ ___| (___   ___  ___   /  \  _   _| |_ ___| (___  _ __ | | ___  _| |_ '
+echo -e ' |  ___/ |__/ _ \\\___ \ / _ \/ __| / /\ \| | | | __/ _ \\\___ \| |_ \| |/ _ \| | __|'
+echo -e ' | |   | | | (_) |___) |  __/ (__ / ____ \ |_| | || (_) |___) | |_) | | (_) | | |_ '
+echo -e ' |_|   |_|  \___/_____/ \___|\___/_/    \_\__,_|\__\___/_____/| .__/|_|\___/|_|\__|'
+echo -e '                                                              | |                  '
+echo -e '                                                              |_|                  '
+echo -e ''
+
 IP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6)
-echo '! >> own IP eth0: '$IP
+#echo '! >> own IP eth0: '$IP
 
 if [[ -d /root/output/msf  && -d /root/input/msf ]]; then
-    echo '! > Folder exist!'
+    #echo '! > Folder exist!'
 else    
     #Creating Output Folders
     mkdir -p /root/output/msf
@@ -22,12 +32,12 @@ fi
 
 read -p "Enter Workspace Name: " WS
 
-echo $WS
+#echo $WS
 
 echo "workspace -d $WS" > /root/input/msf/workspace.txt
 echo "workspace -a $WS" >> /root/input/msf/workspace.txt
 echo "db_import /root/output/nmap/service.xml" >> /root/input/msf/workspace.txt
 
 echo '! > Start Metasploit Framework'
-msfconsole -x "resource /root/input/msf/workspace.txt resource ./resource.txt"
+msfconsole -qx "resource /root/input/msf/workspace.txt resource ./resource.txt" > /dev/null
 echo '! > Done!'
