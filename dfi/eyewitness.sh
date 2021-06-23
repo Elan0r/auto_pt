0b414234bbd7f8517baf4b439fa94bf9d045686b
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-06-23 12:50:40 +0000

    Update eyewitness.sh

--- dfi/eyewitness.sh
+++ dfi/eyewitness.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -d /root/output/nmap -a -d /root/output/list -a -d /root/input/msf -a -d /root/output/loot -a -d /root/output/msf ]; then
+    echo '! > Folder Exist!'
+else    
+    #Creating Output Folders
+    mkdir -p /root/output/nmap /root/output/list /root/input/msf /root/output/loot /root/output/msf
+    #echo '! > Folder Created!'
+fi
+
 if [ -d /root/output/screens ]]; then
     echo '! > Folder exist; mv screens screens_old for Backup purpose!'
     mv /root/output/screens /root/output/screens_old
