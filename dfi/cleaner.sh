fe39f2e2b1986df1e2c1d6a1b05771464bb0ed85
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-07-06 13:51:11 +0000

    addes PCredz

--- dfi/cleaner.sh
+++ dfi/cleaner.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "Deleting Metasploit Workspaces"
-
+echo "  _____           _____            _____ _                            "
+echo " |  __ \         / ____|          / ____| |                           "
+echo " | |__) | __ ___| (___   ___  ___| |    | | ___  __ _ _ __   ___ _ __ "
+echo " |  ___/ '__/ _ \\___ \ / _ \/ __| |    | |/ _ \/ _` | '_ \ / _ \ '__|"
+echo " | |   | | | (_) |___) |  __/ (__| |____| |  __/ (_| | | | |  __/ |   "
+echo " |_|   |_|  \___/_____/ \___|\___|\_____|_|\___|\__,_|_| |_|\___|_|   "
+echo ""
+echo "! > Cleaning all!"
 msfconsole -qx 'workspace -D; exit'
-
-echo "Deleting Folders and Files"
 
 rm -r /root/input
 rm -r /root/output
 rm -r /root/.cme
 rm -r /usr/share/responder/logs
+rm -r /opt/PCredz/logs
+rm /opt/PCredz/CredentialDump-Session.log
 rm /root/.msf4/history
 rm /root/.zsh_history
-rm /root/bash_history
+rm /root/.bash_history
 
-echo "CLEAN"
+echo "! >CLEAN"
