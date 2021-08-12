94f9e42e32d4db27cb2dab17330aa255a96dd192
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-08-12 10:28:40 +0000

    ssdp

--- dfi/looter.sh
+++ dfi/looter.sh
@@ -128,3 +128,7 @@ mkdir -p /root/output/loot/intern/mail/pop3
 awk '/\+.*:110/ {print$2}' /root/output/msf/mail.txt | cut -d ":" -f 1 | sort -u > /root/output/loot/intern/mail/pop3/hosts.txt
 mkdir -p /root/output/loot/intern/mail/relay
 awk '/Potential open SMTP relay/ {print$2}' /root/output/msf/mail.txt | cut -d ":" -f 1 | sort -u > /root/output/loot/intern/mail/relay/hosts.txt
+
+### SSDP
+mkdir -p /root/output/loot/intern/ssdp/amp
+awk '/x Amplification/ {print$2}' /root/output/msf/ssdp.txt | cut -d ":" -f 1 | sort -u > /root/output/loot/intern/ssdp/amp/hosts.txt
