8886bc64865dd84c423e8735447399490338ca4d
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-08-25 07:10:14 +0000

    Update passive_recon.sh

--- dfi/passive_recon.sh
+++ dfi/passive_recon.sh
@@ -18,14 +18,8 @@ fi
 tmux -f /opt/hacking/dfi/dfitmux.conf new-session -d
 tmux rename-window 'Passive-Recon'
 
-echo -e '' > /dev/pts/1
-echo "  _____           _____           _____              _           _____                      " > /dev/pts/1
-echo " |  __ \         / ____|         |  __ \            (_)         |  __ \                     " > /dev/pts/1
-echo " | |__) | __ ___| (___   ___  ___| |__) |_ _ ___ ___ ___   _____| |__) |___  ___ ___  _ __  " > /dev/pts/1
-echo " |  ___/ '__/ _ \\\\___ \ / _ \/ __|  ___/ _\` / __/ __| \ \ / / _ \  _  // _ \/ __/ _ \| '_ \ " > /dev/pts/1
-echo " | |   | | | (_) |___) |  __/ (__| |  | (_| \__ \__ \ |\ V /  __/ | \ \  __/ (_| (_) | | | |" > /dev/pts/1
-echo " |_|   |_|  \___/_____/ \___|\___|_|   \__,_|___/___/_| \_/ \___|_|  \_\___|\___\___/|_| |_|" > /dev/pts/1
-echo '' > /dev/pts/1
+figlet ProSecPassiveRecon > /dev/pts/1
+figlet ProSecPassiveRecon > /dev/pts/0
 
 tmux send 'netdiscover -L -i eth0 >> /root/output/netdiscover' ENTER
 tmux split-window
