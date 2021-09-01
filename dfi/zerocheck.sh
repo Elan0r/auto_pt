0883e6019eba02c133793e67bec8765c8a8b288f
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-09-01 12:55:59 +0000

    Update zerocheck.sh

--- dfi/zerocheck.sh
+++ dfi/zerocheck.sh
@@ -10,28 +10,25 @@ else
     #echo '! > Folder Created!'
 fi
 
-if [ -s /root/input/msf/ws.txt ] || [ /root/output/msf/zerohosts.txt ]; then
-    echo '! >Workspace already set or zerohosts available!'
-else
-    read -p "Enter Workspace Name: " WS
-    echo 'workspace -a ' $WS > /root/input/msf/ws.txt
-    echo 'hosts -S Windows -c name,address -o /root/output/msf/zerohosts.txt' > /root/input/msf/zerohosts.txt
-    echo 'exit' >> /root/input/msf/zerohosts.txt
-fi
-
 if [ -s /root/output/list/zero.txt ]; then
     echo '! > list zero.txt available!'
 else
+
+    if [ -s /root/input/msf/ws.txt ]; then
+        echo '! >Workspace already set!'
+    else
+        read -p "Enter Workspace Name: " WS
+        echo 'workspace -a ' $WS > /root/input/msf/ws.txt
+        echo 'hosts -S Windows -c name,address -o /root/output/msf/zerohosts.txt' > /root/input/msf/zerohosts.txt
+        echo 'exit' >> /root/input/msf/zerohosts.txt
+    fi
+
     msfconsole -qx "resource /root/input/msf/ws.txt resource /root/input/msf/zerohosts.txt" > /dev/null
     awk '/"/ {print}' /root/output/msf/zerohosts.txt | grep -v '""' | cut -d '"' -f 2,4 | sed 's/"/ /' > /root/output/list/zero.txt
 fi
 
-if [ -s /root/output/list/zero.txt ]; then
-    printf '%sspool /root/output/msf/zerologon.txt\necho "ZeroLogon"\nuse auxiliary/admin/dcerpc/cve_2020_1472_zerologon\n' > /root/input/msf/zerologon.txt
-    awk '// {printf"\nset nbname "$1"\nset rhosts "$2"\ncheck\n"}' /root/output/list/zero.txt >> /root/input/msf/zerologon.txt
-    printf '%s\nexit\n' >> /root/input/msf/zerologon.txt
-    msfconsole -qx "resource /root/input/msf/ws.txt resource /root/input/msf/zerologon.txt" > /dev/null
-    echo '! > Check Done!'
-else
-    echo '! > Check not possible no Targets /root/output/list/zero.txt'
-fi
+printf '%sspool /root/output/msf/zerologon.txt\necho "ZeroLogon"\nuse auxiliary/admin/dcerpc/cve_2020_1472_zerologon\n' > /root/input/msf/zerologon.txt
+awk '// {printf"\nset nbname "$1"\nset rhosts "$2"\ncheck\n"}' /root/output/list/zero.txt >> /root/input/msf/zerologon.txt
+printf '%s\nexit\n' >> /root/input/msf/zerologon.txt
+msfconsole -qx "resource /root/input/msf/ws.txt resource /root/input/msf/zerologon.txt" > /dev/null
+echo '! > Check Done!'
