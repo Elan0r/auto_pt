7a9b94f14593dc3dc1d7c8deda66c0bc43c1373c
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-09-06 09:31:22 +0000

    log + txt files in /root/

--- dfi/cleaner.sh
+++ dfi/cleaner.sh
@@ -9,10 +9,14 @@ rm -r /root/output
 rm -r /root/.cme
 rm -r /usr/share/responder/logs
 rm -r /opt/PCredz/logs
+mkdir /opt/PCredz/logs
 rm -r /root/.msf4/loot
 rm /opt/PCredz/CredentialDump-Session.log
 rm /root/.msf4/history
 rm /root/.zsh_history
 rm /root/.bash_history
+rm /root/*.log
+rm /root/*.txt
+rm /root/bettercap.history
 
 echo "! > CLEAN!"
