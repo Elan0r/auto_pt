3dcf401bedd9cc669df710fa89c6430bf8f85db8
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-09-17 08:44:25 +0000

    Add new file

---
+++ dfi/tool_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+figlet ProSecToolz
+echo '! > Tools go to /opt + ln -s to /root/tools'
+
+#APT
+apt install docker.io python3-pip libpcap-dev yersinia -y
+#PIP3!
+pip3 install Cython python-libpcap bloodhound
+
+#GIT
+cd /opt
+git clone https://gitlab-ci-token:ZGA2PFZZyut_zXevuPAR@gitlab.com/pspt/hacking.git
+git clone https://github.com/topotam/PetitPotam.git
+git clone https://github.com/dmaasland/proxyshell-poc.git
+git clone https://github.com/Shadow0ps/CVE-2021-21974.git
+git clone https://github.com/maxpl0it/CVE-2020-1350-DoS.git
+git clone https://github.com/lgandx/PCredz.git
+git clone https://github.com/EmpireProject/Empire.git
+git clone https://github.com/cddmp/enum4linux-ng.git
+git clone https://github.com/RUB-NDS/PRET.git
+git clone https://github.com/ropnop/kerbrute.git
+ln -s /opt /root/tools
+
+#docker
+docker pull empireproject/empire
+git clone https://github.com/byt3bl33d3r/ItWasAllADream.git
+cd /opt/ItWasAllADream && docker build -t itwasalladream .
+exit 0
