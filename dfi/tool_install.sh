f8587e5a4d260e31cbf448d6a69e336bd97d5f5f
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-09-29 10:30:36 +0000

    Check for tools via if implemented

--- dfi/tool_install.sh
+++ dfi/tool_install.sh
@@ -29,43 +29,147 @@ export PATH=$PATH:$GOPATH/bin
 
 
 #GIT
-#git clone https://gitlab-ci-token:ZGA2PFZZyut_zXevuPAR@gitlab.com/pspt/hacking.git
-cd /opt/hacking
-git stash
-git pull
-cd /opt
-git clone https://github.com/topotam/PetitPotam.git
-git clone https://github.com/dmaasland/proxyshell-poc.git
-git clone https://github.com/Shadow0ps/CVE-2021-21974.git
-git clone https://github.com/maxpl0it/CVE-2020-1350-DoS.git
-git clone https://github.com/lgandx/PCredz.git
-git clone https://github.com/EmpireProject/Empire.git
-git clone https://github.com/cddmp/enum4linux-ng.git
-git clone https://github.com/RUB-NDS/PRET.git
-git clone https://github.com/ropnop/kerbrute.git
+/cd /opt
+#PSN Toolz
+if [ -d /opt/hacking ]; then
+    cd /opt/hacking
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://gitlab-ci-token:ZGA2PFZZyut_zXevuPAR@gitlab.com/pspt/hacking.git
+fi
+#Petit Potam
+if [ -d /opt/PetitPotam ]; then
+    cd /opt/PetitPotam
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/topotam/PetitPotam.git
+fi
+#Proxyshell
+if [ -d /opt/proxyshell-poc ]; then
+    cd /opt/proxyshell-poc
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/dmaasland/proxyshell-poc.git
+fi
+#ESXi OpenSLP heap-overflow
+if [ -d /opt/CVE-2021-21974 ]; then
+    cd /opt/CVE-2021-21974
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/Shadow0ps/CVE-2021-21974.git
+fi
+#SIGRed
+if [ -d /opt/CVE-2020-1350-DoS ]; then
+    cd /opt/CVE-2020-1350-DoS
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/maxpl0it/CVE-2020-1350-DoS.git
+fi
+#PCredz
+if [ -d /opt/PCredz ]; then
+    cd /opt/PCredz
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/lgandx/PCredz.git
+fi
+#Empire
+if [ -d /opt/Empire ]; then
+    cd /opt/Empire
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/EmpireProject/Empire.git
+fi
+#Enum4linux NG
+if [ -d /opt/enum4linux-ng ]; then
+    cd /opt/enum4linux-ng
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/cddmp/enum4linux-ng.git
+fi
+#Printer Exploitation Toolkit
+if [ -d /opt/PRET ]; then
+    cd /opt/PRET
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/RUB-NDS/PRET.git
+fi
+#Kerbrute
+if [ -d /opt/kerbrute ]; then
+    cd /opt/kerbrute
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/ropnop/kerbrute.git
+fi
 
 #Go
+cd /opt
 go get github.com/ropnop/kerbrute
 go get github.com/ropnop/go-windapsearch
 go get github.com/sensepost/gowitness
 
-#special
-git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
-cd /opt/airgeddon && bash airgeddon.sh
-cd /opt
-
-git clone https://github.com/ExAndroidDev/impacket.git
-cd impacket
-git checkout ntlmrelayx-adcs-attack
-python3 -m venv impacket
-source impacket/bin/activate
-pip install .
-deactivate
-cd /opt
-
 #docker
-docker pull empireproject/empire
-git clone https://github.com/byt3bl33d3r/ItWasAllADream.git
-cd /opt/ItWasAllADream && docker build -t itwasalladream .
+#Empire Docker
+cd /opt/Empire
+    docker pull empireproject/empire
+#Printnightmare Check
+if [ -d /opt/ItWasAllADream ]; then
+    cd /opt/ItWasAllADream
+    git stash
+    git pull
+    docker build -t itwasalladream .
+else
+    cd /opt
+    git clone https://github.com/byt3bl33d3r/ItWasAllADream.git
+    cd /opt/ItWasAllADream
+    docker build -t itwasalladream .
+fi
+
+#Special
+#impacket ADCS
+if [ -d /opt/impacket ]; then
+    cd /opt/impacket
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/ExAndroidDev/impacket.git
+    cd impacket
+    git checkout ntlmrelayx-adcs-attack
+    python3 -m venv impacket
+    source impacket/bin/activate
+    pip install .
+    deactivate
+fi
+#airgeddon
+if [ -d /opt/airgeddon ]; then
+    cd /opt/airgeddon
+    git stash
+    git pull
+else
+    cd /opt
+    git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
+    cd /opt/airgeddon
+    bash airgeddon.sh
+fi
 
 exit 0
