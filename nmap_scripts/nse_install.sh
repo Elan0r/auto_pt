08f673bef5f10527e592437043532d6e1478f220
D F <8780724-Elan0r@users.noreply.gitlab.com>
2021-11-16 12:31:08 +0000

    added Vulners.nse

--- nmap_scripts/nse_install.sh
+++ nmap_scripts/nse_install.sh
@@ -2,6 +2,19 @@
 
 cp /opt/hacking/nmap_scripts/*.nse /usr/share/nmap/scripts/
 
+#vulscan
+if [ -d /opt/nmap-vulners ]; then
+    cd /opt/nmap-vulners
+    git stash
+    git pull
+else
+    cd /opt
+    git clone https://github.com/vulnersCom/nmap-vulners.git
+fi
+cp /opt/nmap-vulners/vulners.nse /usr/share/nmap/scripts/
+cp /opt/nmap-vulners/http-vulners-regex.nse /usr/share/nmap/scripts/
+cp /opt/nmap-vulners/http-vulners-regex.json /usr/share/nmap/nselib/data/
+cp /opt/nmap-vulners/http-vulners-paths.txt /usr/share/nmap/nselib/data/
 nmap --script-updatedb
 
 exit0
