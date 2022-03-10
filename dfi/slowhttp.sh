29cba1d7aa15285fa99f750921625e3fe9181214
D F <8780724-Elan0r@users.noreply.gitlab.com>
2022-03-10 10:20:55 +0000

    slow http Test

---
+++ dfi/slowhttp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+figlet ProSecSlowHTTP
+
+echo -e ''
+read -p 'File with Domains for SlowHTTP (no http/https): ' file
+if [ -s $file ]; then
+    echo '! > FILE OK '
+else
+    echo "! >> NO File"
+	exit 1
+fi
+
+echo -e ''
+read -p 'Where to save? (no end / ): ' folder
+if [ ! -d $folder ]
+then
+	mkdir -p $folder
+	echo -e '! > Folder Created at '$folder
+else
+	echo -e '! > Folder OK!'
+fi
+time=10
+echo -e ''
+read -p 'Test Time in SECONDS (10 is default) : ' time
+
+for i in $(cat $file) 
+do 
+slowhttptest -l $time -g -o $folder/$i -u https://$i -c 9999 -r 2000 -b 240
+sleep 5 
+done
+
+exit 0
