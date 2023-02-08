3fca5e722da3cc5c961828e00069c3f1433c1181
Elan0r <8780724-Elan0r@users.noreply.gitlab.com>
2023-02-08 15:40:54 +0000

    CI/CD

--- dns_enum.sh
+++ dns_enum.sh
@@ -4,7 +4,7 @@ figlet ProSecDNSenum
 echo "this could take some time!"
 
 #request Nameserver IP
-read -p "Customer INTERNAL DNS Server IP no subnetmask: " NS
+read -p -r "Customer INTERNAL DNS Server IP no subnetmask: " NS
 
 #Private IP range
 echo "10.0.0.0/8
@@ -16,14 +16,14 @@ echo 'Start DNS Enum' >>/root/output/runtime.txt
 date >>/root/output/runtime.txt
 
 #IP Networks via DNS
-nmap -sL --dns-servers $NS -iL /root/input/privip.txt -oN /root/output/nmap/dns.nmap  >/dev/null 2>&1
+nmap -sL --dns-servers "$NS" -iL /root/input/privip.txt -oN /root/output/nmap/dns.nmap >/dev/null 2>&1
 
 #make input file for auto_pt
 awk '/\(1/ {print$6}' /root/output/nmap/dns.nmap | sed 's/[()]//g' >/root/output/list/dnsup.txt
 if [ -s /root/input/ipint.txt ]; then
-  cat /root/output/list/dnsup.txt | cut -d . -f 1,2,3 | sort -u | sed 's/$/.0\/24/' >/root/input/ipint.txt
+  cut -d . -f 1,2,3 /root/output/list/dnsup.txt | sort -u | sed 's/$/.0\/24/' >/root/input/ipint.txt
 else
-  cat /root/output/list/dnsup.txt | cut -d . -f 1,2,3 | sort -u | sed 's/$/.0\/24/' >/root/input/dnsipint.txt
+  cut -d . -f 1,2,3 /root/output/list/dnsup.txt | sort -u | sed 's/$/.0\/24/' >/root/input/dnsipint.txt
 fi
 
 echo 'END DNS Enum' >>/root/output/runtime.txt
--- user_checks.sh
+++ user_checks.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 figlet -w 84 ProSecUserChecks
 echo "v0.7"
 echo "CME is still buggy u need to press ENTER sometimes!"
@@ -63,7 +63,7 @@ fi
 # get DC_FQDN
 FQDN=$(nslookup "$IP" | awk '// {print$4}' | sed 's/.$//')
 # Domain all uppercase for BH query
-BHDOM=$(echo "$DOM" | tr [:lower:] [:upper:])
+BHDOM=$(echo "$DOM" | tr '[:lower:]' '[:upper:]')
 
 if [ -z "$HASH" ]; then
   # Use Password
@@ -71,96 +71,96 @@ if [ -z "$HASH" ]; then
   echo "GPP_Password" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # GPP password
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM -M gpp_password >>/root/output/loot/intern/ad/gpp_password/pass_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" -M gpp_password >>/root/output/loot/intern/ad/gpp_password/pass_"$DOM".txt
    
-  awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_password/pass_$DOM.txt | sort -u >/root/output/loot/intern/ad/gpp_password/host.txt
+  awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_password/pass_"$DOM".txt | sort -u >/root/output/loot/intern/ad/gpp_password/host.txt
   if [ -s /root/output/loot/intern/ad/gpp_password/host.txt ]; then
     echo "PS-TN-2020-0051 GPP_Password" >>/root/output/loot/intern/findings.txt
-    awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_password/pass_$DOM.txt | sort -u >>/root/output/loot/intern/findings.txt
+    awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_password/pass_"$DOM".txt | sort -u >>/root/output/loot/intern/findings.txt
   fi
 
   echo "GPP_Autologin" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # GPP autologin
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM -M gpp_autologin >>/root/output/loot/intern/ad/gpp_autologin/login_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" -M gpp_autologin >>/root/output/loot/intern/ad/gpp_autologin/login_"$DOM".txt
    
-  awk '/Found credentials/ {print$2}' /root/output/loot/intern/ad/gpp_autologin/login_$DOM.txt | sort -u >/root/output/loot/intern/ad/gpp_autologin/host.txt
+  awk '/Found credentials/ {print$2}' /root/output/loot/intern/ad/gpp_autologin/login_"$DOM".txt | sort -u >/root/output/loot/intern/ad/gpp_autologin/host.txt
   if [ -s /root/output/loot/intern/ad/gpp_autologin/host.txt ]; then
     echo "PS-TN-2021-0002 GPP_Autologin" >>/root/output/loot/intern/findings.txt
-    awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_autologin/login_$DOM.txt | sort -u >>/root/output/loot/intern/findings.txt
+    awk '/Found credentials in/ {print$2}' /root/output/loot/intern/ad/gpp_autologin/login_"$DOM".txt | sort -u >>/root/output/loot/intern/findings.txt
   fi
 
   echo "username_as_pass" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # User txt from DC
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM --users >/root/output/list/raw.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" --users >/root/output/list/raw.txt
   awk '/445/ {print$5}' /root/output/list/raw.txt | cut -d '\' -f 2 | grep -v 'HealthMailbox' | sed '/\x1b\[[0-9;]*[mGKHF]/d' >/root/output/list/user.txt
   rm /root/output/list/raw.txt
-  crackmapexec smb $IP -u /root/output/list/user.txt -p /root/output/list/user.txt --no-bruteforce --continue-on-success >>/root/output/loot/intern/ad/iam/username/raw_$DOM.txt
-  grep '+' /root/output/loot/intern/ad/iam/username/raw_$DOM.txt >/root/output/loot/intern/ad/iam/username/user_$DOM.txt
+  crackmapexec smb "$IP" -u /root/output/list/user.txt -p /root/output/list/user.txt --no-bruteforce --continue-on-success >>/root/output/loot/intern/ad/iam/username/raw_"$DOM".txt
+  grep '+' /root/output/loot/intern/ad/iam/username/raw_"$DOM".txt >/root/output/loot/intern/ad/iam/username/user_"$DOM".txt
   # keep the raw file for screens and debugging
   # BH owned User
-  awk '/\+/ {print$6}' /root/output/loot/intern/ad/iam/username/user_$DOM.txt | cut -d : -f 2 | sort -u | tr [:lower:] [:upper:] >/root/output/loot/intern/ad/iam/username/owneduser_$BHDOM.txt
+  awk '/\+/ {print$6}' /root/output/loot/intern/ad/iam/username/user_"$DOM".txt | cut -d : -f 2 | sort -u | tr '[:lower:]' '[:upper:]' >/root/output/loot/intern/ad/iam/username/owneduser_"$BHDOM".txt
 
-  for i in $(cat /root/output/loot/intern/ad/iam/username/owneduser_$BHDOM.txt); do
-    echo "MATCH (n) WHERE n.name = '$i@$BHDOM' SET n.owned=true RETURN n;" >>/root/output/loot/intern/ad/iam/username/bh_owned_$BHDOM.txt
+  for i in $(cat /root/output/loot/intern/ad/iam/username/owneduser_"$BHDOM".txt); do
+    echo "MATCH (n) WHERE n.name = '"$i"@"$BHDOM"' SET n.owned=true RETURN n;" >>/root/output/loot/intern/ad/iam/username/bh_owned_"$BHDOM".txt
   done
 
   echo "Pass-Pol" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # Password policy
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM --pass-pol >>/root/output/loot/intern/ad/passpol/pol_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" --pass-pol >>/root/output/loot/intern/ad/passpol/pol_"$DOM".txt
 
   echo "nopac" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # nopac
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM -M nopac >>/root/output/loot/intern/ldap/nopac/nopac_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" -M nopac >>/root/output/loot/intern/ldap/nopac/nopac_"$FQDN".txt
 
   echo "Petit Potam" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # petitpotam
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM -M petitpotam >>/root/output/loot/intern/rpc/petit_potam/petitpotam_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" -M petitpotam >>/root/output/loot/intern/rpc/petit_potam/petitpotam_"$FQDN".txt
 
   echo "DFScoerce" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # DFScoerce
-  crackmapexec smb $IP -u $USER -p $PASS -d $DOM -M dfscoerce >>/root/output/loot/intern/rpc/dfscoerce/dfscoerce_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -p "$PASS" -d "$DOM" -M dfscoerce >>/root/output/loot/intern/rpc/dfscoerce/dfscoerce_"$FQDN".txt
 
   echo "Sessions" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # sessions
-  crackmapexec smb /root/output/list/smb_open.txt -u $USER -p $PASS -d $DOM --sessions >>/root/output/loot/intern/ad/session/sessions_$DOM.txt
+  crackmapexec smb /root/output/list/smb_open.txt -u "$USER" -p "$PASS" -d "$DOM" --sessions >>/root/output/loot/intern/ad/session/sessions_"$DOM".txt
 
   echo "ASRep" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # asreproast
-  crackmapexec ldap $FQDN -u $USER -p $PASS -d $DOM --asreproast /root/output/loot/intern/ad/kerberos/asreproast/asrep_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -p "$PASS" -d "$DOM" --asreproast /root/output/loot/intern/ad/kerberos/asreproast/asrep_"$DOM".txt
 
   echo "Kerberoast" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # kerberoast
-  crackmapexec ldap $FQDN -u $USER -p $PASS -d $DOM --kerberoasting /root/output/loot/intern/ad/kerberos/kerberoasting/krb_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -p "$PASS" -d "$DOM" --kerberoasting /root/output/loot/intern/ad/kerberos/kerberoasting/krb_"$DOM".txt
 
   echo "MAQ" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # MAQ  
-  crackmapexec ldap $FQDN -u $USER -p $PASS -d $DOM -M MAQ >>/root/output/loot/intern/ad/quota/maq_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -p "$PASS" -d "$DOM" -M MAQ >>/root/output/loot/intern/ad/quota/maq_"$DOM".txt
 
   echo "LDAP Signing with CME" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # CME Ldap signing
-  crackmapexec ldap $FQDN -u $USER -p $PASS -d $DOM -M ldap-checker >>/root/output/loot/intern/ldap/signing/ldap_check_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -p "$PASS" -d "$DOM" -M ldap-checker >>/root/output/loot/intern/ldap/signing/ldap_check_"$DOM".txt
 
   echo "LDAP Signing LdapRelayScan" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # ldap signing 
-  python3 /opt/LdapRelayScan/LdapRelayScan.py -u $USER -p $PASS -dc-ip $IP -method BOTH >>/root/output/loot/intern/ldap/signing/signig_$DOM.txt
+  python3 /opt/LdapRelayScan/LdapRelayScan.py -u "$USER" -p "$PASS" -dc-ip "$IP" -method BOTH >>/root/output/loot/intern/ldap/signing/signig_"$DOM".txt
 
   echo "Bloodhound + Certipy OLD" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # bloodhound
-  bloodhound-python -u $USER -p $PASS -d $DOM -dc $FQDN -w 50 -c all --zip
-  certipy find -u $USER -p $PASS -target $IP -old-bloodhound
+  bloodhound-python -u "$USER" -p "$PASS" -d "$DOM" -dc "$FQDN" -w 50 -c all --zip
+  certipy find -u "$USER" -p "$PASS" -target "$IP" -old-bloodhound
   mv *.zip /root/output/loot/intern/ad
 
   # Python unbuffered reset to default
@@ -170,90 +170,90 @@ if [ -z "$HASH" ]; then
   exit 0
 fi
 
-if [ -z $PASS ]; then
+if [ -z "$PASS" ]; then
   # use HASH
   export PYTHONUNBUFFERED=TRUE
   echo "GPP_Password" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # GPP password
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM -M gpp_password >>/root/output/loot/intern/ad/gpp_password/pass_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" -M gpp_password >>/root/output/loot/intern/ad/gpp_password/pass_"$DOM".txt
 
   echo "GPP_Autologin" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # GPP autologin
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM -M gpp_autologin >>/root/output/loot/intern/ad/gpp_autologin/login_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" -M gpp_autologin >>/root/output/loot/intern/ad/gpp_autologin/login_"$DOM".txt
 
   echo "username_as_pass" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # User txt from DC
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM --users >/root/output/list/raw.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" --users >/root/output/list/raw.txt
   awk '/445/ {print$5}' /root/output/list/raw.txt | cut -d '\' -f 2 | grep -v 'HealthMailbox' | sed '/\x1b\[[0-9;]*[mGKHF]/d' >/root/output/list/user.txt
   rm /root/output/list/raw.txt
-  crackmapexec smb $IP -u /root/output/list/user.txt -p /root/output/list/user.txt --no-bruteforce --continue-on-success >>/root/output/loot/intern/ad/iam/username/raw_$DOM.txt
-  grep '+' /root/output/loot/intern/ad/iam/username/raw_$DOM.txt >/root/output/loot/intern/ad/iam/username/user_$DOM.txt
+  crackmapexec smb "$IP" -u /root/output/list/user.txt -p /root/output/list/user.txt --no-bruteforce --continue-on-success >>/root/output/loot/intern/ad/iam/username/raw_"$DOM".txt
+  grep '+' /root/output/loot/intern/ad/iam/username/raw_"$DOM".txt >/root/output/loot/intern/ad/iam/username/user_"$DOM".txt
   # keep the raw file for screens and debugging
   # BH owned User
-  awk '/\+/ {print$6}' /root/output/loot/intern/ad/iam/username/user_$DOM.txt | cut -d : -f 2 | sort -u | tr [:lower:] [:upper:] >/root/output/loot/intern/ad/iam/username/owneduser.txt
+  awk '/\+/ {print$6}' /root/output/loot/intern/ad/iam/username/user_"$DOM".txt | cut -d : -f 2 | sort -u | tr '[:lower:]' '[:upper:]' >/root/output/loot/intern/ad/iam/username/owneduser.txt
 
   for i in $(cat /root/output/loot/intern/ad/iam/username/owneduser.txt); do
-    echo "MATCH (n) WHERE n.name = '$i@$BHDOM' SET n.owned=true RETURN n;" >>/root/output/loot/intern/ad/iam/username/bh_owned.txt
+    echo "MATCH (n) WHERE n.name = '"$i"@"$BHDOM"' SET n.owned=true RETURN n;" >>/root/output/loot/intern/ad/iam/username/bh_owned.txt
   done
 
   echo "Pass-Pol" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # Password policy
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM --pass-pol >>/root/output/loot/intern/ad/passpol/pol_$DOM.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" --pass-pol >>/root/output/loot/intern/ad/passpol/pol_"$DOM".txt
 
   echo "nopac" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # nopac
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM -M nopac >>/root/output/loot/intern/ldap/nopac/nopac_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" -M nopac >>/root/output/loot/intern/ldap/nopac/nopac_"$FQDN".txt
 
   echo "Petit Potam" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # petitpotam
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM -M petitpotam >>/root/output/loot/intern/rpc/petit_potam/petitpotam_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" -M petitpotam >>/root/output/loot/intern/rpc/petit_potam/petitpotam_"$FQDN".txt
 
   echo "DFScoerce" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # DFScoerce
-  crackmapexec smb $IP -u $USER -H $HASH -d $DOM -M dfscoerce >>/root/output/loot/intern/rpc/dfscoerce/dfscoerce_$FQDN.txt
+  crackmapexec smb "$IP" -u "$USER" -H "$HASH" -d "$DOM" -M dfscoerce >>/root/output/loot/intern/rpc/dfscoerce/dfscoerce_"$FQDN".txt
 
   echo "Sessions" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # sessions
-  crackmapexec smb /root/output/list/smb_open.txt -u $USER -H $HASH -d $DOM --sessions >>/root/output/loot/intern/ad/session/sessions_$FQDN.txt
+  crackmapexec smb /root/output/list/smb_open.txt -u "$USER" -H "$HASH" -d "$DOM" --sessions >>/root/output/loot/intern/ad/session/sessions_"$FQDN".txt
 
   echo "ASRep" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # asreproast
-  crackmapexec ldap $FQDN -u $USER -H $HASH -d $DOM --asreproast /root/output/loot/intern/ad/kerberos/asreproast/asrep_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -H "$HASH" -d "$DOM" --asreproast /root/output/loot/intern/ad/kerberos/asreproast/asrep_"$DOM".txt
 
   echo "Kerberoast" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # kerberoast
-  crackmapexec ldap $FQDN -u $USER -H $HASH -d $DOM --kerberoasting /root/output/loot/intern/ad/kerberos/kerberoasting/krb_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -H "$HASH" -d "$DOM" --kerberoasting /root/output/loot/intern/ad/kerberos/kerberoasting/krb_"$DOM".txt
 
   echo "MAQ" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # MAQ  
-  crackmapexec ldap $FQDN -u $USER -H $HASH -d $DOM -M MAQ >>/root/output/loot/intern/ad/quota/maq_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -H "$HASH" -d "$DOM" -M MAQ >>/root/output/loot/intern/ad/quota/maq_"$DOM".txt
 
   echo "LDAP Signing with CME" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # CME Ldap signing
-  crackmapexec ldap $FQDN -u $USER -H $HASH -d $DOM -M ldap-checker >>/root/output/loot/intern/ldap/signing/ldap_check_$DOM.txt
+  crackmapexec ldap "$FQDN" -u "$USER" -H "$HASH" -d "$DOM" -M ldap-checker >>/root/output/loot/intern/ldap/signing/ldap_check_"$DOM".txt
 
   echo "LDAP Signing LdapRelayScan" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # ldap signing
-  python3 /opt/LdapRelayScan/LdapRelayScan.py -u $USER -nthash $HASH -dc-ip $IP -method BOTH >/root/output/loot/intern/ldap/signing/signig_$DOM.txt
+  python3 /opt/LdapRelayScan/LdapRelayScan.py -u "$USER" -nthash "$HASH" -dc-ip "$IP" -method BOTH >/root/output/loot/intern/ldap/signing/signig_"$DOM".txt
 
   echo "Bloodhound + Certipy OLD" >>/root/output/runtime.txt
   date >>/root/output/runtime.txt
   # bloodhound
-  bloodhound-python -u $USER --hashes aad3b435b51404eeaad3b435b51404ee:$HASH -d $DOM -dc $FQDN -w 50 -c all --zip
-  certipy find -u $USER -hashes $HASH -target $IP -old-bloodhound
+  bloodhound-python -u "$USER" --hashes aad3b435b51404eeaad3b435b51404ee:"$HASH" -d "$DOM" -dc "$FQDN" -w 50 -c all --zip
+  certipy find -u "$USER" -hashes "$HASH" -target "$IP" -old-bloodhound
   mv *.zip /root/output/loot/intern/ad
 
   # Python unbuffered reset to default
