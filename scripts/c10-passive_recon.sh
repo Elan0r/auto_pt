7fdda6ac431788611e38da58927f5532f44b0598
Elan0r <8780724-Elan0r@users.noreply.gitlab.com>
2023-05-04 10:16:04 +0000

    py3.10 not needed

--- scripts/c10-passive_recon.sh
+++ scripts/c10-passive_recon.sh
@@ -8,7 +8,7 @@ date >>/root/output/runtime.txt
 tmux new-window -n:Passive-Recon
 tmux send 'netdiscover -p -L -i eth0 | tee -a /root/output/netdiscover.txt' ENTER
 tmux split-window
-tmux send 'python3.10 /opt/PCredz/Pcredz -i eth0 -ctv' ENTER
+tmux send 'python3 /opt/PCredz/Pcredz -i eth0 -ctv' ENTER
 tmux split-window
 tmux send 'timeout 300 tcpdump -i eth0 -w /root/output/loot/intern/network/passive.pcap' ENTER
 tmux last-window
