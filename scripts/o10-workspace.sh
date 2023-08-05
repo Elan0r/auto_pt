5197662d0e82dea36526869caf2628d129fe817e
Elan0r <8780724-Elan0r@users.noreply.gitlab.com>
2023-08-05 12:23:06 +0000

    Update 3 files
    - /resources/resource.txt
    - /scripts/o10-workspace.sh
    - /menu.sh

--- scripts/o10-workspace.sh
+++ scripts/o10-workspace.sh
@@ -8,4 +8,23 @@ echo 'db_import /root/output/nmap/service.xml' >>/root/input/msf/workspace.txt
 #for Zerocheck
 echo 'workspace -a ' "$WS" >/root/input/msf/ws.txt
 
+figlet "Mail Address for relay"
+read -r -p "enter internal e-mail" INMAIL
+if [[ $INMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+  echo "Valid email format"
+else
+  echo "Invalid email format"
+  exit 1
+fi
+sed -i "s/INTERN@EXAMPLE.COM/$INMAIL/g" /opt/auto_pt/resources/resource.txt
+
+read -r -p "enter external e-mail" EXMAIL
+if [[ $EXMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+  echo "Valid email format"
+else
+  echo "Invalid email format"
+  exit 1
+fi
+sed -i "s/EXTERN@EXAMPLE.COM/$EXMAIL/g" /opt/auto_pt/resources/resource.txt
+
 figlet 'Workspace Set'
