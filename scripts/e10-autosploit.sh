18a0cb38fa0075de614174d0988c176d6fb01226
Elan0r <8780724-Elan0r@users.noreply.gitlab.com>
2023-09-07 12:40:48 +0000

    Update 2 files
    - /scripts/e10-autosploit.sh
    - /scripts/o10-workspace.sh

--- scripts/e10-autosploit.sh
+++ scripts/e10-autosploit.sh
@@ -9,24 +9,6 @@ else
   exit 1
 fi
 
-# Check for SHD_MANAGER
-if grep -Fxq SHD_MANAGER /usr/share/metasploit-framework/data/wordlists/ipmi_users.txt; then
-  echo '! > SHD_Manager exists'
-else
-  echo 'SHD_MANAGER' >>/usr/share/metasploit-framework/data/wordlists/ipmi_users.txt
-fi
-
-if [ -s /root/input/msf/workspace.txt ]; then
-  echo 'Workspace already set!'
-else
-  read -r -p "Enter Workspace Name: " WS
-  echo 'workspace -d ' "$WS" >/root/input/msf/workspace.txt
-  echo 'workspace -a ' "$WS" >>/root/input/msf/workspace.txt
-  echo 'db_import /root/output/nmap/service.xml' >>/root/input/msf/workspace.txt
-  #for Zerocheck
-  echo 'workspace -a ' "$WS" >/root/input/msf/ws.txt
-fi
-
 echo 'Start autosploit' >>/root/output/runtime.txt
 date >>/root/output/runtime.txt
 
--- scripts/o10-workspace.sh
+++ scripts/o10-workspace.sh
@@ -2,29 +2,35 @@
 
 figlet MSFWorkspace
 
-read -r -p "Enter Workspace Name: " WS
-echo 'workspace -a ' "$WS" >/root/input/msf/workspace.txt
-echo 'db_import /root/output/nmap/service.xml' >>/root/input/msf/workspace.txt
-#for Zerocheck
-echo 'workspace -a ' "$WS" >/root/input/msf/ws.txt
-
-figlet "Mail Address for relay"
-read -r -p "enter internal e-mail: " INMAIL
-if [[ $INMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
-  echo "Valid email format"
+if [ -s /root/input/msf/workspace.txt ]; then
+  echo 'Workspace already set!'
 else
-  echo "Invalid email format"
-  exit 1
-fi
-sed -i "s/INTERN@EXAMPLE.COM/$INMAIL/g" /opt/auto_pt/resources/resource.txt
+  read -r -p "Enter Workspace Name: " WS
+  echo 'workspace -d ' "$WS" >/root/input/msf/workspace.txt
+  echo 'workspace -a ' "$WS" >>/root/input/msf/workspace.txt
+  echo 'db_import /root/output/nmap/service.xml' >>/root/input/msf/workspace.txt
+  #for Zerocheck
+  echo 'workspace -a ' "$WS" >/root/input/msf/ws.txt
 
-read -r -p "enter external e-mail: " EXMAIL
-if [[ $EXMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
-  echo "Valid email format"
-else
-  echo "Invalid email format"
-  exit 1
+  figlet "Mail Address for relay"
+  read -r -p "enter internal e-mail: " INMAIL
+  if [[ $INMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+    echo "Valid email format"
+  else
+    echo "Invalid email format"
+    exit 1
+  fi
+  sed -i "s/INTERN@EXAMPLE.COM/$INMAIL/g" /opt/auto_pt/resources/resource.txt
+
+  read -r -p "enter external e-mail: " EXMAIL
+  if [[ $EXMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+    echo "Valid email format"
+  else
+    echo "Invalid email format"
+    exit 1
+  fi
+  sed -i "s/EXTERN@EXAMPLE.COM/$EXMAIL/g" /opt/auto_pt/resources/resource.txt
+
 fi
-sed -i "s/EXTERN@EXAMPLE.COM/$EXMAIL/g" /opt/auto_pt/resources/resource.txt
 
 figlet 'Workspace Set'
